a3f8ce8df6f9651965c016a1c27165cfbdf9ec00
github username <github email address>
2018-09-10 14:28:21 -0400

    set up the controller with new action - created new form using a partial

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,8 @@
+class RecipesController < ApplicationController
+
+  def new
+    @recipe = Recipe.new 
+  end
+
+
+end
---
+++ app/models/recipe.rb
@@ -0,0 +1,4 @@
+class Recipe < ApplicationRecord
+  belongs_to :user
+  validates :name, presence: true, uniqueness: { scope: :user_id }
+end
